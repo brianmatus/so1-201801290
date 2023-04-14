1a0dd88bc668a2f1159523b116310888fbdc13b6
brianmatus <35672931+brianmatus@users.noreply.github.com>
2023-04-13 19:56:04 -0600

    gRPC client

--- Proyecto1/gRPC/client.js
+++ Proyecto1/gRPC/client.js
@@ -1,7 +1,14 @@
+const mysql = require('mysql');
+require('dotenv').config({ path: './.env.local' });
+////////////////////gRPC Imports///////////////////////
 const grpc = require("@grpc/grpc-js");
 var protoLoader = require("@grpc/proto-loader");
-const PROTO_PATH = "./password.proto";
-const bcrypt = require('bcrypt');
+////////////////////API Imports///////////////////////
+const Hapi = require('@hapi/hapi');
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+const PROTO_PATH = "./vote.proto";
 const options = {
     keepCase: true,
     longs: String,
@@ -9,36 +16,240 @@ const options = {
     defaults: true,
     oneofs: true,
 };
-var grpcObj = protoLoader.loadSync(PROTO_PATH, options);
-const PasswordService = grpc.loadPackageDefinition(grpcObj).PasswordService;
+let grpcObj = protoLoader.loadSync(PROTO_PATH, options);
+const VoteService = grpc.loadPackageDefinition(grpcObj).VoteService;
 
-const clientStub = new PasswordService(
+const clientStub = new VoteService(
     "localhost:50051",
     grpc.credentials.createInsecure()
 );
 
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+let departments = {}
+let municipalities = {}
+let papers = {}
+let parties = {}
 
-clientStub.retrievePasswords({}, (error, passwords) => {
-    //implement your error logic here
-    console.log(passwords);
+
+const MYSQL_HOST = process.env.MYSQL_HOST;
+const MYSQL_PORT = process.env.MYSQL_PORT;
+const MYSQL_DB = process.env.MYSQL_DB;
+const MYSQL_USER = process.env.MYSQL_USER;
+const MYSQL_PASS = process.env.MYSQL_PASS;
+
+const connection = mysql.createConnection({
+    host: MYSQL_HOST,
+    user: MYSQL_USER,
+    password: MYSQL_PASS,
+    database: MYSQL_DB
 });
 
-const saltRounds = 10;
-let passwordToken = "5TgU76W&eRee!";
-let updatePasswordToken = "H7hG%$Yh33"
-bcrypt.genSalt(saltRounds, function (error, salt) {
-    bcrypt.hash(passwordToken, salt, function (error, hash) {
-        clientStub.addNewDetails(
-            {
-                id: Date.now(),
-                password: passwordToken,
-                hashValue: hash,
-                saltValue: salt,
-            },
-            (error, passwordDetails) => {
-                //implement your error logic here
-                console.log(passwordDetails);
-            }
-        );
+
+getVoteInfo()
+
+
+function getVoteInfo() {
+    (async () => {
+        try {
+            connection.connect();
+            await getDeparments();
+            // console.log(departments);
+
+            await getMunicipalities();
+            // console.log(municipalities);
+
+            await getPapers();
+            // console.log(papers);
+
+            await getParties();
+            // console.log(parties);
+
+        } catch (error) {
+            console.error(error);
+        } finally {
+            connection.end();
+        }
+    })();
+}
+
+
+
+
+
+// clientStub.AddNewVote({
+//         sede: 3,
+//         municipio: 1,
+//         departamento: 2,
+//         papeleta: 1,
+//         partido: 6
+//     },
+//     (error, voteDetails) => {
+//         //implement your error logic here
+//         console.log(voteDetails);
+//     }
+// );
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: '0.0.0.0',
+        routes: {
+            cors: true
+        }
     });
-});
+
+    server.route({
+        method: 'POST',
+        path: '/new_vote',
+        handler: (request, h) => {
+
+            const resPromise = new Promise((resolve, _) => {
+                console.log("Vote request received")
+                console.log(request.payload)
+
+                newVote = {}
+                newVote.sede = request.payload.sede
+
+                key = getKeyByDepartment(request.payload.departamento)
+                if (key === -1) {
+                    console.log("No Department found")
+                    resolve({ "status": 11});
+                }
+                newVote["departamento"] = key
+
+                key = getKeyByMunicipality(request.payload.municipio, newVote["departamento"])
+                if (key === -1) {
+                    console.log("No Municipality found")
+                    resolve({ "status": 12});
+                }
+                newVote["municipio"] = key
+                key = getKeyByPaper(request.payload.papeleta)
+                if (key === -1) {
+                    console.log("No Paper found")
+                    resolve({ "status": 13});
+                }
+                newVote["papeleta"] = key
+
+                key = getKeyByParty(request.payload.partido)
+                if (key === -1) {
+                    console.log("No Party found")
+                    resolve({ "status": 14});
+                }
+                newVote["partido"] = key
+
+                console.log("Formatted new vote:")
+                console.log(newVote)
+
+                clientStub.AddNewVote({
+                        sede: newVote.sede,
+                        municipio: newVote.municipio,
+                        departamento: newVote.departamento,
+                        papeleta: newVote.papeleta,
+                        partido: newVote.partido
+                    },
+                    (error, voteDetails) => {
+                        //implement your error logic here
+                        console.log(voteDetails);
+                    }
+                );
+
+                resolve({ "status": 0});
+            });
+            return resPromise.then(data => data);
+        }
+    });
+    await server.start();
+    console.log('Servidor Funcionando en %s', server.info.uri);
+}
+init()
+
+function getKeyByDepartment(name) {
+    for (const key in departments) {
+        if (departments[key].name === name) {
+            return parseInt(key)
+        }
+    }
+    return -1
+}
+
+function getKeyByMunicipality(name, dpt) {
+    for (const key in municipalities) {
+        if (municipalities[key].name === name && municipalities[key].dpt_id === dpt) {
+            return parseInt(key)
+        }
+    }
+    return -1
+}
+
+function getKeyByPaper(name) {
+    for (const key in papers) {
+        if (papers[key].name === name) {
+            return parseInt(key)
+        }
+    }
+    return -1
+}
+
+function getKeyByParty(name) {
+    for (const key in parties) {
+        if (parties[key].name === name) {
+            return parseInt(key)
+        }
+    }
+    return -1
+}
+
+async function getDeparments() {
+    return new Promise((resolve, reject) => {
+        departments = {}
+        connection.query('SELECT * FROM departments', (error, results, fields) => {
+            if (error) throw error;
+            results.forEach(row => {
+                departments[row.ID] = {id:row.ID, name:row.NAME}
+            });
+            resolve(0)
+        });
+    });
+}
+
+async function getMunicipalities() {
+    return new Promise((resolve, reject) => {
+        municipalities = {}
+        connection.query('SELECT * FROM municipalities', (error, results, fields) => {
+            if (error) throw error;
+            results.forEach(row => {
+                municipalities[row.ID] = {id:row.ID, dpt_id:row.DPT, name:row.NAME}
+            });
+            resolve(0)
+        });
+    });
+}
+
+async function getPapers() {
+    return new Promise((resolve, reject) => {
+        papers = {}
+        connection.query('SELECT * FROM papers', (error, results, fields) => {
+            if (error) throw error;
+            results.forEach(row => {
+                papers[row.ID] = {id:row.ID, name:row.NAME}
+            });
+            resolve(0)
+        });
+    });
+}
+
+async function getParties() {
+    return new Promise((resolve, reject) => {
+        parties = {}
+        connection.query('SELECT * FROM parties', (error, results, fields) => {
+            if (error) throw error;
+            results.forEach(row => {
+                parties[row.ID] = {id:row.ID, name:row.NAME}
+            });
+            resolve(0)
+        });
+    });
+}
--- Proyecto1/gRPC/server.js
+++ Proyecto1/gRPC/server.js
@@ -1,6 +1,6 @@
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
-const PROTO_PATH = "./password.proto";
+const PROTO_PATH = "./vote.proto";
 
 
 const loaderOptions = {
@@ -18,29 +18,13 @@ const grpcObj = grpc.loadPackageDefinition(packageDef);
 
 const ourServer = new grpc.Server();
 
-let dummyRecords = {
-    "passwords": [
-        { id: "153642", password: "default1", hashValue: "default", saltValue:       "default" },
-        { id: "234654", password: "default2", hashValue: "default", saltValue: "default" }]
-};
 
-ourServer.addService(grpcObj.PasswordService.service, {
-    /*our protobuf message(passwordMessage) for the RetrievePasswords was Empty. */
-    retrievePasswords: (passwordMessage, callback) => {
-        callback(null, dummyRecords);
-    },
-    addNewDetails: (passwordMessage, callback) => {
-        const passwordDetails = { ...passwordMessage.request };
-        dummyRecords.passwords.push(passwordDetails);
-        callback(null, passwordDetails);
-    },
-    updatePasswordDetails: (passwordMessage, callback) => {
-        const detailsID = passwordMessage.request.id;
-        const targetDetails = dummyRecords.passwords.find(({ id }) => detailsID == id);
-        targetDetails.password = passwordMessage.request.password;
-        targetDetails.hashValue = passwordMessage.request.hashValue;
-        targetDetails.saltValue = passwordMessage.request.saltValue;
-        callback(null, targetDetails);
+ourServer.addService(grpcObj.VoteService.service, {
+    addNewVote: (voteMessage, callback) => {
+        const voteDetails = { ...voteMessage.request };
+        console.log("New vote request: info:")
+        console.log(voteDetails)
+        callback(null, {status : 200});
     },
 });
 
