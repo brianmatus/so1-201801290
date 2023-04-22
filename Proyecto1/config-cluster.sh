338464ba08758caf6d8d72a112f0fe4f571e2cd6
brianmatus <35672931+brianmatus@users.noreply.github.com>
2023-04-21 18:02:57 -0600

    Functional Kubernetes Demo 1.1

---
+++ Proyecto1/config-cluster.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#Linkerd and Viz Dashboard
+linkerd check --pre
+linkerd install --crds | kubectl apply -f -
+linkerd install | kubectl apply -f -
+linkerd check
+linkerd viz install | kubectl apply -f -
+linkerd check
+#linkerd viz dashboard
+
+#Nginx
+nginx-meshctl deploy
+
+
+#Proyecto So1
+kubectl apply -f k-pod3.yaml -f k-pod4.yaml -f k-pod5.yaml -f k-pod1.yaml -f k-pod2.yaml -f k-target-svc.yaml -f k-gateway.yaml
